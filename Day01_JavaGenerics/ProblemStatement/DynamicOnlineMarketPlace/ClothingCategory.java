19d9c35e6141609c03b5d95e17fd6ee84e616f07
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-02-11 13:51:03 +0530

    [Aman] ClothingCategory class implements ProductCategory(interface).

---
+++ Day01_JavaGenerics/ProblemStatement/DynamicOnlineMarketPlace/ClothingCategory.java
@@ -0,0 +1,9 @@
+package Day01_JavaGenerics.ProblemStatement.DynamicOnlineMarketPlace;
+
+public class ClothingCategory implements ProductCategory {
+    @Override
+    public String getCategoryName() {
+        return "Clothing";
+    }
+}
+
