ce4abf54603625b8470ec270b570e56a49a4681a
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-02-11 16:00:40 +0530

    [Aman] VegetarianMeal class implements MealPlan(interface).

---
+++ Day01_JavaGenerics/ProblemStatement/PersonalizedMealPlanGenerator/VegetarianMeal.java
@@ -0,0 +1,8 @@
+package Day01_JavaGenerics.ProblemStatement.PersonalizedMealPlanGenerator;
+
+public class VegetarianMeal implements MealPlan {
+    @Override
+    public String getMealCategory() {
+        return "Vegetarian Meal";
+    }
+}
