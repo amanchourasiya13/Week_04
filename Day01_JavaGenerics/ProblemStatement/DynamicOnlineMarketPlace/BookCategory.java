d806ef8dddf226dce55ab29dc3f912cc0460c70b
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-02-11 13:54:03 +0530

    [Aman] BookCategory class implements ProductCategory(interface).

---
+++ Day01_JavaGenerics/ProblemStatement/DynamicOnlineMarketPlace/BookCategory.java
@@ -0,0 +1,8 @@
+package Day01_JavaGenerics.ProblemStatement.DynamicOnlineMarketPlace;
+
+public class BookCategory implements ProductCategory {
+    @Override
+    public String getCategoryName() {
+        return "Books";
+    }
+}
