0a53d00792c3eae8b0e132a2e7293c1b9dd5121e
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-02-11 13:52:56 +0530

    [Aman] GadgetCategory class implements ProductCategory(interface).

---
+++ Day01_JavaGenerics/ProblemStatement/DynamicOnlineMarketPlace/GadgetCategory.java
@@ -0,0 +1,8 @@
+package Day01_JavaGenerics.ProblemStatement.DynamicOnlineMarketPlace;
+
+public class GadgetCategory implements ProductCategory {
+    @Override
+    public String getCategoryName() {
+        return "Gadgets";
+    }
+}
