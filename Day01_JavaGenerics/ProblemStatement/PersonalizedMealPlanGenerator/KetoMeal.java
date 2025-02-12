a60cba0b67f45eec0380dd083afdb2a6fd299c84
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-02-12 12:38:05 +0530

    [Aman] KetoMeal class implements MealPlan(interface).

---
+++ Day01_JavaGenerics/ProblemStatement/PersonalizedMealPlanGenerator/KetoMeal.java
@@ -0,0 +1,8 @@
+package Day01_JavaGenerics.ProblemStatement.PersonalizedMealPlanGenerator;
+
+public class KetoMeal implements  MealPlan {
+    @Override
+    public String getMealCategory(){
+        return "Keto Meal";
+    }
+}
